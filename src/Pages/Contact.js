e4f41e7960c79514d50393370b51076f0060c94f
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2021-05-02 21:03:51 +0600

    contact form done

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,10 +1,27 @@
 import React from 'react';
 
+
 const Contact = () => {
+  
     return (
-        <div>
-            <h1>Contact Page</h1>
+        <section className="contact-form">
+        <div className="contact-section">
+            <h2>Contact Me</h2>
+            <form action="" style={{fontSize: '18px'}} >
+                <label for="name">Name: </label>
+                <input type="text" placeholder="Your name..." required />
+                <label for="email">Email Address: </label>
+                <input type="email" placeholder="Your email..." required/>
+                <label for="name">Subject: </label>
+                <input type="text" placeholder="Subject..." required />
+                <label for="text">Message:</label>
+                <textarea name="message" id="message" cols="30" rows="6" required></textarea>
+
+                <input type="submit" value="Submit"/>
+            </form>
         </div>
+
+    </section>
     );
 };
 
